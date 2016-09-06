e0a8454ec10bb49b1b16097791e4baeeff19377c
Oleg Taranenko <olegtaranenko@gmail.com>
2016-09-06 20:26:09 +0200

    minor change to splash

--- bin/lib/shared_functions.sh
+++ bin/lib/shared_functions.sh
@@ -4,24 +4,6 @@ function parse_opt() {
  echo "parse_opt"
 }
 
-#function _submodule_git_commit() {
-#  name=$1
-#  path=$2
-#  toplevel=$3
-#  sha1=$4
-#  echo "$name $toplevel/$path $sha1"
-#
-#  cat /tmp/_submodule_git_commit.params | args
-#  cd $toplevel/$path
-##  git add . -A
-##  git commit -am "******** first commit via submodules"
-##  echo ${args[*]}
-##  git commit ${args[*]}
-#  return 0
-#}
-#
-#export -f _submodule_git_commit
-
 function consistentwork_bootstrap () {
   local gitDir
   local umbrellaRepoDir
@@ -45,26 +27,26 @@ function consistentwork_bootstrap () {
 
   local url=$(get_repo_url "$umbrellaRepoDir")
   local cwUrl=${url%%gitadmin@git.consistentwork.com:*}
-  if [[ -nz ${cwUrl} ]]; then
+  if [[ -n ${cwUrl} ]]; then
     cw_echo ${cwUrl}
     die
   fi
 
   rm ${cwTmpSubmodules} &>/dev/null
-  splash ${url}
   pushd ${umbrellaRepoDir} &>/dev/null
   umbrellaOriginUrl=$(git remote get-url origin)
   umbrellaRepoName=${umbrellaOriginUrl##*:}
   umbrellaRepoName=${umbrellaRepoName##*/}
   git submodule  foreach --recursive  |  tail  -r | sed "s/[^']*//" | tr -d "'" >>  ${cwTmpSubmodules}
 
-  popd ${umbrellaRepoDir} &>/dev/null
 
   #globals is an array defined in the caller of this method
   globals[0]=${umbrellaRepoDir}
   globals[1]=${cwTmpSubmodules}
   globals[2]=${gitDir}
   globals[3]=${umbrellaRepoName}
+  popd &>/dev/null
+  splash ${url}
 
   return 0
 }
@@ -115,18 +97,25 @@ function splash() {
   (( $skipSplash )) && return
   local url=$1
   url=${url##*:}
-  local msg="umbrella repo: \"${url}\""
-  currentUrl=$(get_repo_url ".")
-  if [[ ${currentUrl} != ${url} ]]; then
-    msg+="; current: \"${currentUrl##*:}\""
-  fi
+  url=${url##*/}
+  local msg="umbrella repo: "${url}
+  local currentGitDir=$(get_repo_git_dir)
+  currentRepoName=${currentGitDir##*/modules/}
+  [[ -n ${currentRepoName} && ${url} != ${currentRepoName} ]] && msg+=", current module '"${currentRepoName}"'"
   cw_echo "${msg}"
 }
 
 function get_repo_url() {
-  [[ $1 == "." ]] || pushd $1  > /dev/null
+  [[ $1 != "." ]] && pushd $1  > /dev/null
   local url=$(git remote get-url origin)
-  [[ $1 == "." ]] || popd  > /dev/null
+  [[ $1 != "." ]] && popd  > /dev/null
+  echo ${url}
+}
+
+function get_repo_git_dir() {
+  [[ -n $1 && $1 != "." ]] && pushd $1  > /dev/null
+  local url=$(git rev-parse --git-dir)
+  [[ -n $1 && $1 != "." ]] && popd  > /dev/null
   echo ${url}
 }
 
