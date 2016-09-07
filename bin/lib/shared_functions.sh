8386a742ae091f1b005437f03ff1834a2ce31347
Oleg Taranenko <olegtaranenko@gmail.com>
2016-09-08 00:10:10 +0200

    module_porcelain_status refactoring

--- bin/lib/shared_functions.sh
+++ bin/lib/shared_functions.sh
@@ -150,3 +150,101 @@ function is_branch_exists() {
   echo ${ret}
   return ${ret}
 }
+
+## Constants
+typeset MODULE_STATUS=()
+MS_BRANCH_INFO=0
+MS_DETACHED=1
+MS_COMMITABLE=2
+MS_UNTRACKED=3
+MS_MODIFIED=4
+MS_DELETED=5
+MS_ADDED=6
+MS_RENAMED=7
+MS_COPIED=8
+MS_UNMERGED=9
+
+#
+##  Get status for current module
+#
+#     Filling up $MODULE_STATUS array with parsed information about current module state.
+#     Information in the array will be valid till next call of the method.
+#     Directory of module should be set outside this function.
+#
+## Parameters
+#     none
+#
+## Globals affected
+#     $MODULE_STATUS array
+#
+function module_porcelain_status () {
+#  set -x
+
+#  local pwd
+#  if [[ -n $1 && $1 != "." && $1 != "/" && $1 != ".." ]]; then
+#    pwd=$1
+#    pushd $pwd  &> /dev/null
+#  elif [[ $1 == "/" ]]; then
+#    pwd=${globals[0]}
+#    pushd $pwd  &> /dev/null
+#  else
+#    pwd=$(pwd)
+#  fi
+  unset MODULE_STATUS
+
+  while read -a output; do
+    case ${output[0]} in
+      \#\#)
+        if [[ -n ${output[1]} ]]; then
+          if [[ ${output[1]} == "HEAD" ]]; then
+            MODULE_STATUS[$MS_DETACHED]=1
+            possibleBranch=$(possible_branch_from_parent_config)
+            if [[ -n $possibleBranch ]]; then
+              MODULE_STATUS[$MS_BRANCH_INFO]="detached (${possibleBranch}?)"
+            fi
+          fi
+          [[ -z ${MODULE_STATUS[$MS_BRANCH_INFO]} ]] && MODULE_STATUS[$MS_BRANCH_INFO]="${output[@]:1}"
+        fi
+      ;;
+      *)
+        cmd=${output:0:2}
+        case ${cmd} in
+          \?\?)
+            MODULE_STATUS[$MS_UNTRACKED]=1
+          ;;
+          *)
+            [[ ${cmd:0:1} == "M" || ${cmd:1:1} == "M" ]] && MODULE_STATUS[$MS_MODIFIED]=1; MODULE_STATUS[$MS_COMMITABLE]=1
+            [[ ${cmd:0:1} == "A" || ${cmd:1:1} == "A" ]] && MODULE_STATUS[$MS_ADDED]=1; MODULE_STATUS[$MS_COMMITABLE]=1
+            [[ ${cmd:0:1} == "D" || ${cmd:1:1} == "D" ]] && MODULE_STATUS[$MS_DELETED]=1; MODULE_STATUS[$MS_COMMITABLE]=1
+            [[ ${cmd:0:1} == "R" || ${cmd:1:1} == "R" ]] && MODULE_STATUS[$MS_RENAMED]=1; MODULE_STATUS[$MS_COMMITABLE]=1
+            [[ ${cmd:0:1} == "C" || ${cmd:1:1} == "C" ]] && MODULE_STATUS[$MS_COPIED]=1; MODULE_STATUS[$MS_COMMITABLE]=1
+            [[ ${cmd:0:1} == "U" || ${cmd:1:1} == "U" ]] && MODULE_STATUS[$MS_UNMERGED]=1; MODULE_STATUS[$MS_COMMITABLE]=1
+          ;;
+        esac
+      ;;
+    esac
+  done < <(git status -u --porcelain -b)
+
+#  [[ -n $1 && $1 != "." ]] && popd  &> /dev/null
+#  set +x
+}
+
+function module_up() {
+  cd $(git rev-parse --show-toplevel)"/.."
+  cd $(git rev-parse --show-toplevel)
+}
+
+function possible_branch_from_parent_config() {
+  local ret repoName
+  repoName=$(get_repo_name_from_path)
+  if [[ $repoName != "/" ]]; then
+    pushd . &> /dev/null
+
+    module_up
+    ret=$(git config -f .gitmodules --get "submodule.$repoName.branch")
+    popd &> /dev/null
+  else
+    ret=/
+  fi
+  echo $ret
+}
