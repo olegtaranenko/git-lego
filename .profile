18ca65064410d442b0bb4616712ba2a46c6b1203
Oleg Taranenko <olegtaranenko@gmail.com>
2017-09-30 10:34:06 +0200

    git lego instead umbrella started
    Join mfs.sh & shared_functions.sh into git-lego.
    Because of weird error 'No such file or directory' by process substitution issue

--- .profile
+++ .profile
@@ -7,21 +7,16 @@ fi
 export PATH="~/.consistentwork/bin:$CWORK_HOME/bin:$PATH"
 
 
-
-if [ -f /opt/local/etc/profile.d/.git-completion.bash ]; then
-  source /opt/local/etc/profile.d/.git-completion.bash
-fi
-
 # preference to locally built git
 # see https://gist.github.com/digitaljhelms/2931522 or https://github.com/git/git/blob/master/INSTALL how to build
 # option make prefix=~ is being used
 #
 ## WARNING: no use self-compiled git, because git subrepo is broken in this case
 #
-if [ -f ~/bin/nogit ]; then
-  if [ "$(git --version)" != "$(~/bin/git --version)" ]; then
-    export PATH="~/bin:$PATH"
-  fi
-fi
+#if [ -f ~/bin/nogit ]; then
+#  if [ "$(git --version)" != "$(~/bin/git --version)" ]; then
+#    export PATH="~/bin:$PATH"
+#  fi
+#fi
 
 . $HOME/.consistentwork/bin/lib/mfs.sh
---
+++ git-lego-install.sh
@@ -0,0 +1,149 @@
+#!/usr/bin/env bash
+
+
+	# load common functionality
+#	. "$GITFLOW_DIR/gitflow-common"
+
+	# This environmental variable fixes non-POSIX getopt style argument
+	# parsing, effectively breaking git-flow subcommand parsing on several
+	# Linux platforms.
+	export POSIXLY_CORRECT=1
+
+	# use the shFlags project to parse the command line arguments
+#	. "$GITFLOW_DIR/gitflow-shFlags"
+	FLAGS_PARENT="git flow"
+
+# The sed expression here replaces all backslashes by forward slashes.
+# This helps our Windows users, while not bothering our Unix users.
+export GITLEGO_DIR=$(dirname "$(echo "$0" | sed -e 's,\\,/,g')")
+
+# MacPorts place git sources to /opt/local/share/git folder
+SHARED_GIT_DIR=/opt/local/share/git
+COMPLETION_DIR=${SHARED_GIT_DIR}/contrib/completion
+COMPLETION_SH=${COMPLETION_DIR}/git-completion.bash
+# PROMPT_SH=${COMPLETION_DIR}/git-prompt.sh
+
+#if [ -f ${COMPLETION_SH} ]; then
+#  source ${COMPLETION_SH}
+#fi
+
+
+
+typeset verbose=0
+#typeset PROFILE=0
+typeset force=0
+
+show_help() {
+  cat << EOF
+
+	${0##*/} [.profile|.bash_profile|.bashrc|... any other shell script ...]
+
+        Install environment for git lego to specified profile file.
+	    By default (no profile given) script will look into first existing files in above list
+	    Note, that profile has to be executable.
+
+EOF
+}
+
+#set -x
+while [[ -n $1 ]]; do
+  case $1 in
+    -h|-\?|--help)
+      show_help
+      exit
+      ;;
+    -v|--verbose)
+      verbose=$((verbose + 1)) # Each -v argument adds 1 to verbosity.
+      ;;
+
+    --force|-f)
+      force=1
+      ;;
+    --no-force|-F)
+      force=0
+      ;;
+
+    *)
+      if [[ -z ${1%%-*} ]]; then
+        echo "Unknown option '$1'"
+    	exit 1
+      elif [[ -z ${PROFILE} ]]; then
+        PROFILE=$1
+      fi
+      ;;
+  esac
+  shift
+done
+
+# Arbitrary profile, maybe .bash_profile or .profile or something else...
+if [[ -n ${PROFILE} ]]; then
+  if [[ ! ${PROFILE} = '~/' ]]; then
+    PROFILE="~/"${PROFILE}
+  fi
+  if [[ ! -x ${PROFILE} ]]; then
+    unset profile
+  fi
+fi
+
+[[ -z ${PROFILE} ]] && PROFILE=~/.profile
+[[ -z ${PROFILE} ]] && [[ -f ~/.bash_profile ]] && PROFILE=~/.bash_profile
+
+
+SHARED_GIT_DIR=/opt/local/share/git
+COMPLETION_DIR=${SHARED_GIT_DIR}/contrib/completion
+COMPLETION_SH=${COMPLETION_DIR}/git-completion.bash
+# PROMPT_SH=${COMPLETION_DIR}/git-prompt.sh
+
+#echo ${PROFILE}
+
+
+if [ -f ${COMPLETION_SH} ]; then
+  if [ ! -x ${COMPLETION_SH} ]; then
+    echo "${COMPLETION_SH} does not have executable flag."
+    echo "You maybe required enter admin password"
+    sudo chmod +x ${COMPLETION_SH}
+  fi
+
+  if [[ -f "$PROFILE" ]] && grep -q "${COMPLETION_SH}" "$PROFILE"; then
+	echo "git-completion.bash already added to the profile."
+  else
+	echo "Adding git-completion.bash to the profile..."
+    cat << EOT >> ${PROFILE}
+
+# added by ${0##*/}
+. ${COMPLETION_SH}
+
+EOT
+
+  fi
+
+fi
+
+exit
+
+CW_PROFILE=~/.consistentwork/.profile
+
+(( $verbose )) && echo "profile='${PROFILE}'"
+if [[ -e ${PROFILE} ]] || (( $force )); then
+  env | grep -s CWORK_HOME > /dev/null
+  if (( ! $? )); then
+    echo "CWORK_HOME environment is already installed to '$CWORK_HOME'" >&2
+  else
+    echo "CWORK_HOME environment is not installed, by default will be used ~/consistentwork" >&2
+    echo "export CWORK_HOME=~/consistentwork" >> ${PROFILE}
+  fi
+  echo $PATH | grep -E -o -q "\.consistentwork\/bin" > /dev/null
+  if (( $? )); then
+    cat << EOF >> ${PROFILE}
+
+if [[ -d ~/.consistentwork && -f ${CW_PROFILE} ]]; then
+  . ${CW_PROFILE}
+fi
+EOF
+
+  else
+    (( $verbose )) && echo "PATH already in order" >&2
+  fi
+else
+  printf "${PROFILE} not exists. \nTry '${0##*/} --force' to create a bash profile file \nor '${0##*/} --help' for more information\n" >&2
+fi
